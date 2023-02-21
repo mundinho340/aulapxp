7fbb000c453aa75092d713b10a815fdf6e987a0b
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-20 21:42:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $n = 12;
+        $no ='Raimundo';
 
-echo "<h1>ola mundo</h1>";
+        echo $no ,' ',$n;
+    ?>
+</body>
+</html>
