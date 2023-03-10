861c82e40d8d9dc0ed5e4ffdfbbdb231aa644872
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-03-10 19:12:35 -0300

    op

--- EXERCICIOS2/ex01.php
+++ EXERCICIOS2/ex01.php
@@ -1,6 +1,21 @@
 //array do tipo vector e de tipo matriz
 <?php
     
+    $n= array(
+        array('nome', 'nota1', 'nota2', 'media'),
+        array('raimundo',10, 10,10),
+        array('linda', 10,10,10),
+        array('lucas', 10,10,10)
 
+    );
 
+echo "<table border=1 width=400>";
+    foreach($n as $i=> $valor){
+        echo "<tr>";
+        foreach($n[$i] as $valor){
+            echo "<td>". $valor."</td>";
+        }
+        echo "</tr>";
+
+    }
 ?>
